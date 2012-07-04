0f666ac303a24e1e3600f804f986e7b45f2a83f4
Petr Holasek <pholasek@redhat.com>
2012-07-04 12:34:11 +0200

    remaining tests were added to scripts

--- Makefile
+++ Makefile
@@ -15,9 +15,14 @@ all: numa01 numa02 nmstat
 numa01: numa01.prep.c
 	$(CC) $< $(CFLAGS) -o $@
 	$(CC) $< $(CFLAGS) -DTHREAD_ALLOC -o numa01_THREAD_ALLOC
+	$(CC) $< $(CFLAGS) -DNO_BIND_FORCE_SAME_NODE -o numa01_NO_BIND_FORCE_SAME_NODE
+	$(CC) $< $(CFLAGS) -DHARD_BIND -o numa01_HARD_BIND
+	$(CC) $< $(CFLAGS) -DHARD_BIND -DINVERSE_BIND -o numa01_INVERSE_BIND
 numa02: numa02.prep.c
 	$(CC) $< $(CFLAGS) -o $@
 	$(CC) $< $(CFLAGS) -DSMT -o numa02_SMT
+	$(CC) $< $(CFLAGS) -DHARD_BIND -o numa02_HARD_BIND
+	$(CC) $< $(CFLAGS) -DHARD_BIND -DINVERSE_BIND -o numa02_INVERSE_BIND
 nmstat: nmstat.c
 	$(CC) $< -std=gnu99 -o $@
 clean: 
--- start_bench.sh
+++ start_bench.sh
@@ -13,9 +13,13 @@
 
 usage()
 {
-	echo -e "./start_bench.sh [-sth]"
+	echo -e "./start_bench.sh [-stnbiA] [-h]"
 	echo -e "\t-s : run numa02_SMT test additionally"
 	echo -e "\t-t : run numa01_THREAD_ALLOC test additionally"
+	echo -e "\t-n : run numa01_NO_BIND_FORCE_SAME_NODE test additionally"
+	echo -e "\t-b : run {numa01,numa02}_HARD_BIND tests additionally"
+	echo -e "\t-i : run {numa01,numa02}_INVERSE_BIND tests additionally"
+	echo -e "\t-A : run all available tests"
 	echo -e "\t-h : this help"
 }
 
@@ -44,10 +48,30 @@ run_bench()
 		TESTNAME=numa01_THREAD_ALLOC
 		run_test
 	fi
+	if [ $NBFSN -eq 1 ] ; then
+		TESTNAME=numa01_NO_BIND_FORCE_SAME_NODE
+		run_test
+	fi
+	if [ $HARDBIND -eq 1 ] ; then
+		TESTNAME=numa01_HARD_BIND
+		run_test
+	fi
+	if [ $INVERSEBIND -eq 1 ] ; then
+		TESTNAME=numa01_INVERSE_BIND
+		run_test
+	fi
 	TESTNAME=numa02
 	run_test
 	if [ $SMT -eq 1 ] ; then
 		TESTNAME=numa02_SMT
+		run_test
+	fi
+	if [ $HARDBIND -eq 1 ] ; then
+		TESTNAME=numa02_HARD_BIND
+		run_test
+	fi
+	if [ $INVERSEBIND -eq 1 ] ; then
+		TESTNAME=numa02_INVERSE_BIND
 		run_test
 	fi
 }
@@ -59,14 +83,33 @@ cleanup()
 
 SMT=0
 TALLOC=0
+NBFSN=0
+HARDBIND=0
+INVERSEBIND=0
 
-while getopts "sth" opt; do
+while getopts "stnbiAh" opt; do
 	case $opt in
 		s)
 			SMT=1
 			;;
 		t)
 			TALLOC=1
+			;;
+		n)
+			NBFSN=1
+			;;
+		b)
+			HARDBIND=1
+			;;
+		i)
+			INVERSEBIND=1
+			;;
+		A)
+			SMT=1
+			TALLOC=1
+			NBFSN=1
+			HARDBIND=1
+			INVERSEBIND=1
 			;;
 		h)
 			usage
